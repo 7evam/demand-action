f7f4c2411cd12b5c129c5bf85c9c35dc3616b13a
Evan Lane <evanklane@yahoo.com>
2020-06-05 18:58:37 -0400

    added import graphql in index

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,5 +50,6 @@ module.exports = {
           }
       }
   },
+  `gatsby-transformer-remark`
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import IncidentList from "../components/IncidentList"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
