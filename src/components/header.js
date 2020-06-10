5aea4c40b8ab44c92753f4972b7f5166d85ea7bf
Dan <danhemerlein@gmail.com>
2020-06-09 23:51:52 -0400

    font, modal overlay beginning

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header className="Header p2">
+  <header className="Header p2 raleway">
     <h1 className="Header__headline text-center color-dark-grey">
       <Link to="/">{siteTitle}</Link>
     </h1>
